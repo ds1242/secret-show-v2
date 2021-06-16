84f638bbadae2f91953afa959d0d73c889d656e1
dshaw1242 <david.shaw1242@gmail.com>
2021-06-15 19:26:26 -0600

    added session secret from .env to server.js

---
+++ models/index.js
@@ -0,0 +1,42 @@
+const User = require('./User');
+const Band = require('./Band');
+const Comment = require('./Comment');
+const Role = require('./Role');
+const Show = require('./Show');
+
+
+User.hasMany(Show, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+User.hasOne(Role, {
+    foreignKey: 'role_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Band, {
+    foreignKey: 'band_id'
+});
+
+Comment.belongsTo(Role, {
+    foreignKey: 'role_id'
+});
+
+Role.belongsTo(User, {
+    foreignKey: 'role_id'
+});
+
+Band.belongsTo(Show, {
+    foreignKey: 'band_id'
+});
+
+
+
+module.exports = { User, Band, Comment, Role, Show };
