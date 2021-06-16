c9d234e9aed0893b7f79ce10ce68fc4aac073ce8
dshaw1242 <david.shaw1242@gmail.com>
2021-06-15 18:41:58 -0600

    added foreign key to User model

--- models/User.js
+++ models/User.js
@@ -34,6 +34,14 @@ User.init(
             validate: {
                 isEmail: true
             }
+        },
+        role_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'role',
+                key: 'id'
+            }
         }
     },
     {
