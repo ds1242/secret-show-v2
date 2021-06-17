333b6f0dcfb046ad048c1fc8216ca2b1e7674cce
AliMitchell <ali.mitchell.n@gmail.com>
2021-06-17 17:16:34 -0600

    added neon sign effect/ ladning.js file

---
+++ public/js/landing.js
@@ -0,0 +1,10 @@
+const header = document.getElementById("reflect");
+
+
+
+$(reflect).hover(function(){
+    $(this).attr('src','./images/Secret Show glow.png');
+}, function () {
+    $(this).attr('src','./images/Secret show.png');
+
+  });
