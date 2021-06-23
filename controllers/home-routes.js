6faa0a41e2591fed4e058184bace41ba25e8b46c
dshaw1242 <david.shaw1242@gmail.com>
2021-06-23 07:34:25 -0600

    should have fixed the header so it redirects correctly when logged in or not, header displays on create show correctly now

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,16 +4,16 @@ const { Role, Show, User, Genre, Comment, Band } = require('../models');
 
 // The below, as uncommented, when logged in, can no longer to to main page, only /show. Commented out, you can visit homepage or show page just fine
 router.get('/', (req, res) => {
-    // if(req.session.loggedIn) {
-    //     res.redirect('/')
-    // }
+    if(req.session.loggedIn) {
+        res.redirect('/show')
+    }
     res.render('signup')
 });
 
 router.get('/login', (req, res) => {
-    // if(req.session.loggedIn) {
-    //     res.redirect('/')
-    // }
+    if(req.session.loggedIn) {
+        res.redirect('/show')
+    }
     res.render('login')
 });
 
