c069bb98b1ccf996eb26fda103e0f5def7428e1a
yana-mkr <yana.mkrtchyan@gmail.com>
2021-06-24 19:40:22 -0500

    edit and delete button w/ form adjustments

--- controllers/api/show-routes.js
+++ controllers/api/show-routes.js
@@ -1,29 +1,29 @@
 const router = require('express').Router();
-const { User, Comment, Band, Show} = require('../../models');
+const { User, Comment, Band, Show } = require('../../models');
 
 // Find all shows
 router.get('/', (req, res) => {
     Show.findAll(
         {
-        include: {
-            model: User,
-            attributes: ['id']
-        }
-    })
+            include: {
+                model: User,
+                attributes: ['id']
+            }
+        })
         .then(dbShowData => res.json(dbShowData))
         .catch(err => {
             res.status(500).json({ message: 'Cannot find show' })
         });
 });
 
-router.get('/', (req, res) => {
+router.get('/:id', (req, res) => {
     Show.findOne(
         {
-        include: {
-            model: User,
-            attributes: ['id']
-        }
-    })
+            include: {
+                model: User,
+                attributes: ['id']
+            }
+        })
         .then(dbShowData => res.json(dbShowData))
         .catch(err => {
             res.status(500).json({ message: 'Cannot find show' })
@@ -44,11 +44,62 @@ router.post('/', (req, res) => {
         user_id: req.session.user_id
     })
 
-    .then(dbShowData => res.json(dbShowData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+        .then(dbShowData => res.json(dbShowData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// make changes to show
+router.put('/:id', (req, res) => {
+    Show.update({
+        bandname: req.body.bandname,
+        genre: req.body.genre,
+        img: req.body.img,
+        youtube_id: req.body.youtube_id,
+        show_location: req.body.show_location,
+        show_time: req.body.show_time,
+        show_date: req.body.show_date,
+        privacy: req.body.privacy,
+        user_id: req.session.user_id
+    },
+        {
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(dbShowData => {
+            if (!dbShowData[0]) {
+                res.status(404).json({ message: 'No data found for this show ' });
+                return;
+            }
+            res.json(dbShowData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// delete show
+router.delete('/:id', (req, res) => {
+    Show.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbShowData => {
+            if (!dbShowData) {
+                res.status(404).json({ message: 'No show found with this id' });
+                return;
+            }
+            res.json(dbShowData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
