aa39640745e11c76eb5c3cb5ad1db294bef0b194
dshaw1242 <david.shaw1242@gmail.com>
2021-06-27 19:26:13 -0600

    heroku push

--- public/js/back-button.js
+++ public/js/back-button.js
@@ -1,7 +1,7 @@
 async function createBackButton(event) {
     event.preventDefault();
 
-    document.location.replace('/');
+    document.location.replace('/show');
 }
 
 document.querySelector('#back-to-show-list').addEventListener('click', createBackButton)
